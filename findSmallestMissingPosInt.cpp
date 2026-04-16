dfe7d629425c09cd49dafd0c66dcd9967d5f8d7d
Basha2306 <134443035+basha2123@users.noreply.github.com>
2026-04-16 07:21:54 +0530

    Create findSmallestMissingPosInt.cpp

---
+++ findSmallestMissingPosInt.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+/*
+ * Complete the 'findSmallestMissingPositive' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY orderNumbers as parameter.
+ */
+
+int findSmallestMissingPositive(vector<int> orderNumbers) {
+    int n = orderNumbers.size();
+
+    for (int i = 0; i < n; i++) {
+        while (orderNumbers[i] > 0 && orderNumbers[i] <= n &&
+               orderNumbers[orderNumbers[i] - 1] != orderNumbers[i]) {
+            swap(orderNumbers[i], orderNumbers[orderNumbers[i] - 1]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (orderNumbers[i] != i + 1)
+            return i + 1;
+    }
+
+    return n + 1;
+}
+
+int main()
+{
+    string orderNumbers_count_temp;
+    getline(cin, orderNumbers_count_temp);
+
+    int orderNumbers_count = stoi(ltrim(rtrim(orderNumbers_count_temp)));
+
+    vector<int> orderNumbers(orderNumbers_count);
+
+    for (int i = 0; i < orderNumbers_count; i++) {
+        string orderNumbers_item_temp;
+        getline(cin, orderNumbers_item_temp);
+
+        int orderNumbers_item = stoi(ltrim(rtrim(orderNumbers_item_temp)));
+
+        orderNumbers[i] = orderNumbers_item;
+    }
+
+    int result = findSmallestMissingPositive(orderNumbers);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
