8e3d9d0a8f4e71981bd094ccaa72c72d86663cd7
Basha2306 <134443035+basha2123@users.noreply.github.com>
2026-05-08 10:09:40 +0530

    Create partition-list.cpp

---
+++ partition-list.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+
+        ListNode* smallHead = new ListNode(0);
+        ListNode* largeHead = new ListNode(0);
+
+        ListNode* small = smallHead;
+        ListNode* large = largeHead;
+
+        while (head) {
+
+            if (head->val < x) {
+                small->next = head;
+                small = small->next;
+            }
+            else {
+                large->next = head;
+                large = large->next;
+            }
+
+            head = head->next;
+        }
+
+        large->next = nullptr;
+        small->next = largeHead->next;
+
+        return smallHead->next;
+    }
+};
